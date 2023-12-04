21507726d9bfcfa7927b52fa78735926ca1c7cc2
Rami Nakkar <rami.nakkar@hs-augsburg.de>
2023-12-04 14:17:40 +0100

    feat: create github user model

---
+++ src/models/GitHubUser.ts
@@ -0,0 +1,19 @@
+export interface GitHubUser {
+    login: string;
+    id: number;
+    node_id: string;
+    avatar_url: string;
+    html_url: string;
+    name: string | null;
+    company: string | null;
+    blog: string | null;
+    location: string | null;
+    email: string | null;
+    bio: string | null;
+    public_repos: number;
+    public_gists: number;
+    followers: number;
+    following: number;
+    created_at: string;
+    updated_at: string;
+}
