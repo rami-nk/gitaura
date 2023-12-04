c948de9cd8bf949f3b8db23957b06cae7eebad27
Rami Nakkar <rami.nakkar@hs-augsburg.de>
2023-12-04 10:02:41 +0100

    feat: setup chakra-ui

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.scss'
+import {Button} from "@chakra-ui/react";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -18,9 +19,9 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+          <Button onClick={() => setCount((count) => count + 1)}>
+              count is {count}
+          </Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.scss'
+import {ChakraProvider, ColorModeScript} from '@chakra-ui/react'
+import theme from "./theme.ts";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode}/>
+        <React.StrictMode>
+            <ChakraProvider theme={theme}>
+                <App/>
+            </ChakraProvider>
+        </React.StrictMode>,
+    </>
 )
