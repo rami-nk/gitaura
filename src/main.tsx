09c989d2cb9ff3a2c78f5de42df368a57a940dec
Rami Nakkar <rami.nakkar@hs-augsburg.de>
2023-12-04 10:57:18 +0100

    refactor: remove unnecessary files

--- src/main.tsx
+++ src/main.tsx
@@ -12,6 +12,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
             <ChakraProvider theme={theme}>
                 <App/>
             </ChakraProvider>
-        </React.StrictMode>,
+        </React.StrictMode>
     </>
 )
