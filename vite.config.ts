5cd1b622911bbfecbbc1a321bf042a6fc81ebed8
Rami Nakkar <rami.nakkar@hs-augsburg.de>
2023-12-04 17:35:09 +0100

    test: userSearchInput

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,20 @@
-import { defineConfig } from 'vite'
+/// <reference types="vitest" />
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/gitaura/",
-  build: {
-    outDir: './build'
-  }
+    plugins: [react()],
+    base: "/gitaura/",
+    build: {
+        outDir: './build'
+    },
+    test: {
+        environment: 'jsdom',
+        setupFiles: ['./src/__tests__/setup.ts'],
+        globals: true,
+        coverage: {
+            provider: 'istanbul' // or 'v8'
+        },
+    }
 });
--- vitest.config.ts
+++ vitest.config.ts
@@ -3,8 +3,11 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
     test: {
+        environment: 'jsdom',
+        setupFiles: ['./src/__tests__/setup.ts'],
+        globals: true,
         coverage: {
             provider: 'istanbul' // or 'v8'
         },
-    },
+    }
 })
