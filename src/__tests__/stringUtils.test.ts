fd407b0be5155e4559e9e18dfc0435c8f74a9683
Rami Nakkar <rami.nakkar@hs-augsburg.de>
2023-12-05 15:14:51 +0100

    test: hasNextPage regex

--- src/__tests__/stringUtils.test.ts
+++ src/__tests__/stringUtils.test.ts
@@ -1,4 +1,4 @@
-import { sanitizeClassName } from "../services/stringUtils";
+import {hasNextPage, sanitizeClassName} from "../services/stringUtils";
 
 describe('sanitizeClassName', () => {
     it('replaces # with -Sharp', () => {
@@ -19,5 +19,27 @@ describe('sanitizeClassName', () => {
 
     it('handles a combination of cases', () => {
         expect(sanitizeClassName('C# ++ Language')).toBe('C-Sharp-pp-Language');
+    });
+});
+
+describe('hasNextPage', () => {
+    it('returns true if link contains rel="next"', () => {
+        const link = '<https://api.github.com/user/58343300/repos?per_page=15&page=4>; rel="next"';
+        expect(hasNextPage(link)).toBe(true);
+    });
+
+    it('returns false if link does not contain rel="next"', () => {
+        const link = '<https://api.github.com/user/58343300/repos?per_page=15&page=2>; rel="prev"';
+        expect(hasNextPage(link)).toBe(false);
+    });
+
+    it('returns false for an empty string', () => {
+        const link = '';
+        expect(hasNextPage(link)).toBe(false);
+    });
+
+    it('returns true for multiple links when rel="next" is present', () => {
+        const link = '<https://api.github.com/user/58343300/repos?per_page=15&page=2>; rel="prev", <https://api.github.com/user/58343300/repos?per_page=15&page=4>; rel="next", <https://api.github.com/user/58343300/repos?per_page=15&page=1>; rel="first"';
+        expect(hasNextPage(link)).toBe(true);
     });
 });
