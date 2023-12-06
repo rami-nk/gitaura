c15d90e880b84156f18ec7ac83f798d3c3751d3a
Rami Nakkar <rami.nakkar@hs-augsburg.de>
2023-12-06 12:39:03 +0100

    refactor: extract landing page

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,5 +1,42 @@
+import { VStack } from "@chakra-ui/layout";
+import UserSearchInput from "../components/UserSearchInput.tsx";
+import {useNavigate} from "react-router-dom";
+import {useState} from "react";
+import {getUsers} from "../services/githubService.ts";
+
 const LandingPage = () => {
-    return <div>LandingPage</div>
+
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [error, setError] = useState<string>('');
+
+    const navigate = useNavigate();
+
+    const handleSearch = (username: string) => {
+        setIsLoading(true);
+        getUsers(username)
+            .then(userDataResponse => {
+                navigate(`/repositories/${userDataResponse.data.login}`)
+            })
+            .catch(error => {
+                setError(error.message === "Not Found" ?
+                    `No user with name ${username}.` :
+                    `Error occurred: ${error.message}`);
+            })
+            .finally(() => {
+                setIsLoading(false);
+            });
+    };
+
+    const handleChange = () => {
+        setError("");
+    }
+
+    return (
+        <VStack height="full" w="full" mt={100} align="center">
+            <UserSearchInput onChange={handleChange} isLoading={isLoading} errorMessage={error}
+                             onSearch={handleSearch}/>
+        </VStack>
+    );
 }
 
 export default LandingPage;
